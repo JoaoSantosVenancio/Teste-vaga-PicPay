5641ffd5c8160e506ba163f12ed1d847613181b1
mdJaozin <jv29645@gmail.com>
2023-11-19 22:13:44 -0300

    initial commit

---
+++ src/main/java/com/testePicPay/TestePicPayApplication.java
@@ -0,0 +1,13 @@
+package com.testePicPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePicPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePicPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testePicPay/TestePicPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testePicPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePicPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
